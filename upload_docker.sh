375ceca5f41eab5ffc53f30030884f2932d4c6dd
Lukasz Starsiak <lukaszstarsiak@iMac-Lukasz.local>
2021-02-04 21:43:01 +0100

    add kubernetes deployment

--- upload_docker.sh
+++ upload_docker.sh
@@ -5,11 +5,14 @@
 
 # Step 1:
 # Create dockerpath
-# dockerpath=<your docker ID/path>
+dockerpath=szark/udacity-proj4
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+docker login -u szark -p b1029834-035d-4c05-9eda-5f2e637d99b0 ## take it fromm variable 
+docker image tag api:latest $dockerpath
 
 # Step 3:
 # Push image to a docker repository
+docker push $dockerpath
